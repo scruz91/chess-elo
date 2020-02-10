8d52aca2602102a1dc10006b3cec3427c4e7980c
scruz91 <pscn91@gmail.com>
2020-02-10 09:52:06 -0300

    Updates

---
+++ precache-manifest.f6be497844f78215a4d1f89f4c30d33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9f7ecc1d60e6d9c784e979270e8d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265f5a1ed3ee916fd6f7",
+    "url": "/static/css/main.451e75c9.chunk.css"
+  },
+  {
+    "revision": "20bee13027bc7aabd1b8",
+    "url": "/static/js/2.831fcccc.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.831fcccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265f5a1ed3ee916fd6f7",
+    "url": "/static/js/main.f032ec76.chunk.js"
+  },
+  {
+    "revision": "2ee14adb20ac7afbca6e",
+    "url": "/static/js/runtime-main.e9329a49.js"
+  }
+]);
